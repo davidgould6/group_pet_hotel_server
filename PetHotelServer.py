0ec18881893c6b0b54f87dec88c7b8277a3bedfe
Ashley Berry <ashleywberry@gmail.com>
2020-10-28 14:42:15 -0500

    hello world

--- PetHotelServer.py
+++ PetHotelServer.py
@@ -1,1 +1,11 @@
 # Where our server requests will be
+
+#flask imports
+from flask import Flask, request, jsonify
+#import con from connectionFunction.py
+from connFunction import con
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+  return 'Hello World'
