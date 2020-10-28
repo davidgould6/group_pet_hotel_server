aee5c1268e57dfde494379a5c1f096f0ac9cad36
Ashley Berry <ashleywberry@gmail.com>
2020-10-28 15:20:25 -0500

    queries completed

--- database.sql
+++ database.sql
@@ -17,3 +17,14 @@ VALUES
 ('1', 'Jake', 'adorbs', 'white'), 
 ('2', 'Icarus', 'St. Bernard', 'brown and white'), 
 ('3', 'Boo', 'cat', 'greyish');
+
+SELECT "pets"."id", "pets"."petName", "pets"."breed", "pets"."color", "pets"."isCheckedIn", "owners"."name" AS "ownerName"
+FROM "pets"
+JOIN "owners"
+ON "pets"."owner_id" = "owners"."id";
+
+SELECT "owners"."id", "owners"."name", COUNT("pets"."owner_id")
+FROM "pets"
+JOIN "owners"
+ON "owners"."id" = "pets"."owner_id"
+GROUP BY "owners"."id";
