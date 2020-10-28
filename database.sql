74e07ef5ede1616a7d0bbf247ca1f546d40d0433
Ashley Berry <ashleywberry@gmail.com>
2020-10-28 10:57:05 -0500

    bla bla bla

--- database.sql
+++ database.sql
@@ -1,1 +1,18 @@
 --Where our sql queries will be
+CREATE TABLE "pets"(
+"id" serial PRIMARY KEY,
+"owner_id" INT NOT NULL,
+"petName" VARCHAR (100) NOT NULL,
+"breed" VARCHAR (100) NOT NULL,
+"color" VARCHAR (100) NOT NULL,
+"isCheckedIn" BOOLEAN DEFAULT false
+);
+
+CREATE TABLE "owners"(
+"id" serial PRIMARY KEY,
+"name" VARCHAR (100) NOT NULL
+);
+
+INSERT INTO "owners"("name") VALUES ('David'), ('Gabriel'), ('Ashley');
+
+INSERT INTO "pets"("owner_id", "petName", "breed", "color") VALUES ('1', 'Jake', 'adorbs', 'white'), ('2', 'Icarus', 'St. Bernard', 'brown and white'), ('3', 'Boo', 'cat', 'greyish');
