bb3a42a59e6296dc0d81af5f7240c195c6c5e06c
Gabriel Vargas <vargasgabriel@protonmail.com>
2020-10-28 15:49:24 -0500

    get route working

--- PetHotelServer.py
+++ PetHotelServer.py
@@ -9,3 +9,14 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
   return 'Hello World'
+
+
+@app.route('/pets', methods=['GET', 'POST'])
+def get_pets():
+  if request.method == 'GET':
+    querytext = 'SELECT "pets"."id", "pets"."petName", "pets"."breed", "pets"."color", "pets"."isCheckedIn", "owners"."name" AS "ownerName" FROM "pets" JOIN "owners" ON "pets"."owner_id" = "owners"."id";'
+    cur = con.cursor()
+    cur.execute(querytext)
+    rows = cur.fetchall()
+    print(rows)
+  return 'Pets'
