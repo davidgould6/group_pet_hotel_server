6a5cfdd02454c02abdfd83cba467a606c31c702a
David Gould <davidgould@Davids-MacBook-Pro.local>
2020-10-28 15:52:13 -0500

    testing

--- connFunction.py
+++ connFunction.py
@@ -4,5 +4,5 @@ import psycopg2.extras
 # Connect to db. line 8 allows data to come back as an array with objects inside
 # Found from https://www.psycopg.org/docs/extras.html
 con = psycopg2.connect(
-  "dbname=pet_hotel user=ashleyberry host=localhost", 
+  "dbname=pet_hotel user=davidgould host=localhost", 
   cursor_factory=psycopg2.extras.RealDictCursor)
